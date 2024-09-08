cf47fde0e55a7566d2f4e6b7c4271e8c897ed406
cristian-camilo-dev <cristianrodrig149@gmail.com>
2024-09-08 22:12:10 +0200

    feat(form): add ID unique Activity

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
 import { Activity } from "../types";
 import { ActivityActions } from "../reducers/activityReducer";
@@ -8,6 +9,7 @@ type FormProps = {
 };
 
 const initialState: Activity = {
+  id: uuidv4(),
   category: 1,
   name: "",
   calories: 0,
@@ -35,7 +37,10 @@ export default function Form({ dispatch }: FormProps) {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     dispatch({ type: "save-activity", payload: { newActivity: activity } });
-    setActivity(initialState);
+    setActivity({
+      ...initialState,
+      id: uuidv4(),
+    });
   };
 
   return (
